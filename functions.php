df91cf9343a953609114c8831b5d336987d122bb
carlosapolancoz <carlosapolancoz@gmail.com>
2024-02-18 16:50:28 -0500

    Link styles.css

--- functions.php
+++ functions.php
@@ -7,3 +7,9 @@
     }
 
     add_action('init', 'fitness360_menus'); 
+
+    function fitness360_scripts_styles(){
+        wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0.0');
+    }
+
+    add_action('wp_enqueue_scripts', 'fitness360_scripts_styles');
