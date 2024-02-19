3ca5eae79b48f8bd4e468b0256ad9f4f7bc8956b
carlosapolancoz <carlosapolancoz@gmail.com>
2024-02-19 08:43:20 -0500

    Add page-listado-clases.php

--- page.php
+++ page.php
@@ -2,19 +2,9 @@
     get_header();
 ?>
     <main class="contenedor seccion">
-    <?php
-        while(have_posts()): the_post();
-        
-            the_title('<h1 class="text-center text-primary">', '</h1>');
-
-            if(has_post_thumbnail()) {
-                the_post_thumbnail('full', array('class' => 'imagen-destacada'));
-            }
-
-            the_content();
-
-        endwhile;
-    ?>
+        <?php
+            get_template_part('template-parts/pagina');
+        ?>
     </main>
 <?php
     get_footer();
